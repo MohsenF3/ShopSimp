2124f537315e83dc9e179490ede51c6f01d000d8
Mohsen <3mohsenfaramarzi@gmail.com>
2024-01-21 13:43:05 +0330

    correct spelling

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,7 @@ const Loader = () => {
   return (
     <div className="flex text-center flex-col items-center justify-center w-screen h-screen">
       <h3 className="text-gradient font-mono text-4xl font-bold mb-7">
-        Loding
+        Loading
       </h3>
       <div className="flex flex-row  gap-2  ">
         <div className="w-4 h-4 rounded-full bg-yellow-400 animate-bounce"></div>

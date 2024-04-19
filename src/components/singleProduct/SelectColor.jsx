ccf120cea647d6d7e5214da91ddbd136107f8952
Mohsen <3mohsenfaramarzi@gmail.com>
2024-04-19 14:52:22 +0330

    update ring color in SelectColor component

--- src/components/singleProduct/SelectColor.jsx
+++ src/components/singleProduct/SelectColor.jsx
@@ -1,10 +1,16 @@
 const colors = [
-  { id: 1, color: "blue", bg: "#1376F4" },
-  { id: 3, color: "green", bg: "#2DCC6F" },
-  { id: 5, color: "black", bg: "#000000" },
+  { id: 1, color: "blue" },
+  { id: 3, color: "green" },
+  { id: 5, color: "black" },
 ];
 
 const SelectColor = ({ onColor }) => {
+  const colorVariants = {
+    blue: "bg-brandBlue ring-brandBlue",
+    green: "bg-brandGreen ring-brandGreen",
+    black: "bg-black ring-black",
+  };
+
   return (
     <>
       {colors.map((item) => {
@@ -17,10 +23,9 @@ const SelectColor = ({ onColor }) => {
               defaultChecked={item.color === "blue"}
               value={item.color}
               onChange={onColor}
-              style={{
-                backgroundColor: item.bg,
-              }}
-              className={`appearance-none h-16 w-16 rounded-full ring-offset-1 ring-2 cursor-pointer transition-all duration-200 ease-in-out checked:scale-125 scale-90 hover:scale-125 `}
+              className={`${
+                colorVariants[item.color]
+              } appearance-none h-16 w-16 rounded-full ring-offset-1 ring-2 cursor-pointer transition-all duration-200 ease-in-out checked:scale-125 scale-90 hover:scale-125 `}
             />
             <p className="font-medium capitalize">{item.color}</p>
           </div>

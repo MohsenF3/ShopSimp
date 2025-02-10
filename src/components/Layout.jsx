6a2d666e98afadc490b409e9efb3df2254729d28
MohsenFa <3mohsenfaramarzi@gmail.com>
2025-02-10 11:00:34 +0330

    add font (Rubic)

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,13 +5,13 @@ const Footer = lazy(() => import("./Footer"));
 
 const Layout = () => {
   return (
-    <>
+    <div className="font-rubik antialiased">
       <Navbar />
       <main className="mt-28">
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
       colors: {
         primary: "#f42c37",
         secondary: "#f42c37",
